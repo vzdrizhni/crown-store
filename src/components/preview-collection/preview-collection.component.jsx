f00c91fb47332b1f7b7005d7b3421e7b302c9d5d
vzdrizhni <vzdrizhni@gmail.com>
2020-08-20 22:37:41 +0500

    Update app with a shop page

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './preview-collection.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component'
 
 const PreviewCollection = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.map(item => (
-                <div key={item.id}>{item.name}</div>
+            {items
+            .filter((item, idx) => idx < 4)
+            .map(({id, ...otherPreviewProps}) => (
+                <CollectionItem key={id} {...otherPreviewProps} />
             ))}
         </div>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,12 +14,12 @@ class ShopPage extends React.Component {
     render() {
         const {collections} = this.state;
         return(
-            <div className='shop=page'>
-                {collections.map(({id, ...otherProps }) => (
-                    <PreviewCollection key={id} {...otherProps} />
+            <div className='shop-page'>
+                {collections.map(({id, ...otherCollectionProps }) => (
+                    <PreviewCollection key={id} {...otherCollectionProps} />
                 ))}
             </div>
-        )
+        );
     }
 }
 
