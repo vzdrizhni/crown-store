467b73d011119bed70e0328a33a9239f2955d8f5
vzdrizhni <vzdrizhni@gmail.com>
2020-08-21 16:35:40 +0500

    Add custom button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Switch>
        <Route exact path='/' component={HomePage}/>
        <Route path='/shop' component={ShopPage}/>
+       <Route path='/signin' component={SignInAndSignUp}/>
      </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../../components/signin/sign-in.component'
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-and-sign-up'><SignIn /></div>
+)
+
+export default SignInAndSignUp
