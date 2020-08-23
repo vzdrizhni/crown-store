0c99910d135bef57120c7ca5a0c55026e1ec9667
vzdrizhni <vzdrizhni@gmail.com>
2020-08-23 19:06:47 +0500

    Finish sign-in method

--- src/components/signin/sign-in.component.jsx
+++ src/components/signin/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../components/firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../components/firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+             this.setState({email: '', password: ''})
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     handleChange = (event) => {
@@ -34,7 +41,7 @@ class SignIn extends React.Component {
 
                 <form onSubmit={this.handleSubmit}> 
                     <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} required label='email' />
-                    <FormInput name='password' type='password' value={this.state.email} handleChange={this.handleChange} required label='password' />
+                    <FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange} required label='password' />
                     <CustomButton type="submit" value='submit form'>Sign In</CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn value='submit form'>Sign In With Google</CustomButton>
                 </form>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
 import SignIn from '../../components/signin/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const SignInAndSignUp = () => (
-    <div className='sign-in-and-sign-up'><SignIn /></div>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
 )
 
 export default SignInAndSignUp
