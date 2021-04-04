90fc9cad111c7ebdae293225389ef8e6d90d22d1
Roman <vzdrizhni@gmail.com>
2021-04-04 23:54:25 +0500

    Add spinner component and change request method

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -38,6 +38,24 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+  
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+  
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth();
